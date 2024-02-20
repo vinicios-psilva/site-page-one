20697288778751198f905b653bcc04067dc6adc6
Vinicios Pereira da Silva <97298193+vinicios-psilva@users.noreply.github.com>
2024-02-20 10:56:52 +0000

    Changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,17 @@ const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-sm m-1">
-                <a className="logo" href="/">
-                    <img className="logo" src="logo.png" alt="logo dr. edmilson fernandes" />
-                </a>
+                <Link href="/" legacyBehavior>
+                    <a className="logo">
+                        <Image className="logo" src="/logo.png" width="80" height="80"/>
+                    </a>
+                </Link>
                 <div>
                     <h3 className="text-white m-3">Dr. Edmilson Fernandes</h3>
                     <h6 className="text-white m-3">Núcleo de Aprimoramento Humano</h6>
                 </div>
                 <div className="navbar-collapse justify-content-end" >
-                    <ul className="navbar-nav mr-auto  flex-row d-flex justify-content-center social">
+                    <ul className="navbar-nav mr-auto flex-row d-flex justify-content-center social">
                         <a className="bi bi-instagram" href="https://www.instagram.com/edmilsonfernandesss/" target="_blank"></a>
                         <a className="bi bi-youtube" href="https://www.youtube.com/channel/UCNEKhL1AURk8IJBieAVuaOQ" target="_blank"></a>
                         <a className="bi bi-linkedin" href="https://www.linkedin.com/in/edmilson-fernandes-da-silva-4357b528a/?originalSubdomain=br" target="_blank"></a>
