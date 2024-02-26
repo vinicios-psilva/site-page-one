96ac86f0f2201fadba25bd731cbefe6044e988bc
Vinicios Pereira da Silva <97298193+vinicios-psilva@users.noreply.github.com>
2024-02-26 19:41:10 +0000

    Changed

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,24 +25,24 @@ const Main = () => {
                     </div>
                     <div className="col-md-6 col-sm-4 d-flex justify-content-center">
                         <Image className="img-fluid rounded-3" src="https://static.wixstatic.com/media/73cf8d_7be904d6bf6448f5a17e20bd206e1ecc~mv2.jpg/v1/fill/w_435,h_520,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/73cf8d_7be904d6bf6448f5a17e20bd206e1ecc~mv2.jpg"
-                        width="350" height="350"/>
+                        width="350" height="350" alt=""/>
                     </div>
                 </div>
             </section>
 
             {/* Inicio Depoimentos */}
 
-            <section className="jumbotron p-2 m-4 ">
+            <section className="jumbotron p-2 m-4">
                 <h1 className="text-center py-4 ">Depoimentos</h1>
                 <div className="row p-4 d-flex justify-content-start ">
                     <div className="col-md-3 col-sm-6 depoimento">
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">KAWANE COUTO</h5>
-                                <p>"O início da terapia pode ser estranho, mas com o tempo me senti mais à vontade.
+                                <p>&quot;O início da terapia pode ser estranho, mas com o tempo me senti mais à vontade.
                                     Iniciei para me entender melhor e, sinceramente, não foi tão agradável quanto eu esperava,
                                     mas faz parte do processo. Identificar e compreender meus sentimentos e emoções foi um desafio,
-                                    mas a terapia me ajudou a entender meus limites, a não temer me posicionar e a superar a necessidade de agradar a todos."
+                                    mas a terapia me ajudou a entender meus limites, a não temer me posicionar e a superar a necessidade de agradar a todos.&quot;
                                 </p>
                             </div>
                         </div>
@@ -53,10 +53,10 @@ const Main = () => {
                             <div className="card-body">
                                 <h5 className="card-title">IRLEY SANTOS</h5>
                                 <p>
-                                    "Por muito tempo a terapia foi um grande tabu para mim, eu sempre dizia que não precisava, porque na verdade eu achava que
+                                &quot;Por muito tempo a terapia foi um grande tabu para mim, eu sempre dizia que não precisava, porque na verdade eu achava que
                                     terapia era para gente doida, mas que na verdade é uma conexão de você com você. E o terapeuta Edmilson
                                     faz isso com tanta sutileza que você consegue se enxergar e detectar tudo aquilo que pode somar a mais na sua vida.
-                                    Agora eu entendo que a terapia é um grande bem, então não fique presa ao medo, faça!"
+                                    Agora eu entendo que a terapia é um grande bem, então não fique presa ao medo, faça!&quot;
                                 </p>
                             </div>
                         </div>
@@ -66,11 +66,11 @@ const Main = () => {
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">LARISSA BLACK</h5>
-                                <p>"Quando iniciei a terapia, me senti insegura, mas sabia que precisava de ajuda. Escolher fazer a terapia foi amelhor
+                                <p>&quot;Quando iniciei a terapia, me senti insegura, mas sabia que precisava de ajuda. Escolher fazer a terapia foi amelhor
                                     decisão que tomei.Aprendi a lidar com questões que nunca entenderia sem ela. Edmilson me mostrou queposso lidar comigo mesma.
                                     Além disso, aprendi a lidar com questões familiares que me impediam de ser quem sou hoje.
                                     Este ano foi difícil, especialmente com o falecimento de minha mãe, mas a terapia me ajudou a suportar.
-                                    Só tenho a agradecer!"
+                                    Só tenho a agradecer!&quot;
                                 </p>
                             </div>
                         </div>
@@ -80,10 +80,10 @@ const Main = () => {
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">ISABELLE REITER</h5>
-                                <p>"Nos primeiros dias de terapia, a mente armava armadilhas, tentando interromper o autocuidado.
+                                <p>&quot;Nos primeiros dias de terapia, a mente armava armadilhas, tentando interromper o autocuidado.
                                     Parecia sem sentido abrir minha vida para alguém de fora. Mas aprendi a trazer coisas para o consciente
                                     e aplicar isso em outros aspectos. Sou grata pelo atendimento, pois pude refletir mais e dividir o que depende e não de mim.
-                                    Obrigada, Edmilson, por todos os momentos de aprendizado e reflexão."
+                                    Obrigada, Edmilson, por todos os momentos de aprendizado e reflexão.&quot;
                                 </p>
                             </div>
                         </div>
@@ -101,7 +101,7 @@ const Main = () => {
                 <div className=" row p-4 d-flex align-items-center">
                     <div className="col-md-6 col-sm-4 d-flex justify-content-center ">
                         <Image className="img-fluid rounded img" src="https://static.wixstatic.com/media/73cf8d_954d25b10dbe49149fdf3ab1014de55d~mv2.png/v1/fill/w_391,h_500,al_c,q_85,enc_auto/73cf8d_954d25b10dbe49149fdf3ab1014de55d~mv2.png"
-                        width="350" height="350"/>
+                        width="350" height="350" alt=""/>
                     </div>
                     <div className="col-md-6 col-sm-8">
                         <p className="lead">
@@ -125,11 +125,11 @@ const Main = () => {
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">DAIANE VICENTE</h5>
-                                <p>"Começar a terapia com Edmilson neste ano foi um presente para minha família.
+                                <p>&quot;Começar a terapia com Edmilson neste ano foi um presente para minha família.
                                     Todos estamos beneficiados. Na jornada rumo ao autoconhecimento, estou me libertando de
                                     amarras que atrapalhavam minhas decisões, controle emocional e ansiedade.
                                     Compreendi problemas passados e enfrento desafios diários com firmeza.
-                                    Recomendo muito este excelente profissional."
+                                    Recomendo muito este excelente profissional.&quot;
                                 </p>
                             </div>
                         </div>
@@ -140,10 +140,10 @@ const Main = () => {
                             <div className="card-body">
                                 <h5 className="card-title">LEONARDO CARLOS</h5>
                                 <p>
-                                    "Nessa jornada, aprendi que posso evoluir a cada dia e que meus próprios julgamentos,
+                                &quot;Nessa jornada, aprendi que posso evoluir a cada dia e que meus próprios julgamentos,
                                     ego e emoções não enfrentadas são meus maiores obstáculos. Antes, o medo de abandonar
                                     hábitos me impediu de buscar terapia. Agora, com mais paciência, aprendo a lidar com
-                                    ansiedade, durmo melhor e me sinto mais disposto. Isso renova minha fé e esperança."
+                                    ansiedade, durmo melhor e me sinto mais disposto. Isso renova minha fé e esperança.&quot;
                                 </p>
                             </div>
                         </div>
@@ -153,12 +153,12 @@ const Main = () => {
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">JULYANA PEREIRA</h5>
-                                <p>"Meu nome é Julyana, tenho 28 anos. Há 5 meses, tenho acompanhamento com Edmilson.
+                                <p>&quot;Meu nome é Julyana, tenho 28 anos. Há 5 meses, tenho acompanhamento com Edmilson.
                                     Antes, acreditava na terapia, mas não buscava até sentir a necessidade real.
                                     Sentia-me estagnada, com momentos de pânico e inquietação.
                                     Agora, no processo de autoconhecimento, sou mais calma, capaz de analisar situações e
                                     lidar com a vida com leveza. A terapia mudou minha vida, me fazendo evoluir e lidar melhor
-                                    com as adversidades."
+                                    com as adversidades.&quot;
                                 </p>
                             </div>
                         </div>
@@ -183,7 +183,7 @@ const Main = () => {
                     </div>
                     <div className="col-md-6 col-sm-4 d-flex justify-content-center">
                         <Image className="img-fluid rounded img" src="https://static.wixstatic.com/media/73cf8d_7bedc6061e1d40a78e57d0b604d490b9~mv2.jpg/v1/fill/w_435,h_453,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/73cf8d_7bedc6061e1d40a78e57d0b604d490b9~mv2.jpg"
-                        width="350" height="350"/>
+                        width="350" height="350" alt=""/>
                     </div>
                 </div>
             </section>
@@ -197,10 +197,10 @@ const Main = () => {
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">GABRIELE XAVIER</h5>
-                                <p>"No início, minha timidez e o receio de ser rotulada me impediam de buscar terapia.
+                                <p>&quot;No início, minha timidez e o receio de ser rotulada me impediam de buscar terapia.
                                     Com o tempo, percebi que não se tratava apenas de cura, mas de ressignificar pensamentos e emoções.
                                     A terapia contribuiu significativamente para meu autoconhecimento e saúde mental, apesar de ser
-                                    desconfortável às vezes. O cuidado de Edmilson em cada sessão foi fundamental para minha motivação."
+                                    desconfortável às vezes. O cuidado de Edmilson em cada sessão foi fundamental para minha motivação.&quot;
                                 </p>
                             </div>
                         </div>
@@ -211,10 +211,10 @@ const Main = () => {
                             <div className="card-body">
                                 <h5 className="card-title">JULIO CEZAR</h5>
                                 <p>
-                                    "Nessa jornada, aprendi que posso evoluir a cada dia e que meus próprios julgamentos,
+                                &quot;Nessa jornada, aprendi que posso evoluir a cada dia e que meus próprios julgamentos,
                                     ego e emoções não enfrentadas são meus maiores obstáculos. Antes, o medo de abandonar
                                     hábitos me impediu de buscar terapia. Agora, com mais paciência, aprendo a lidar com
-                                    ansiedade, durmo melhor e me sinto mais disposto. Isso renova minha fé e esperança."
+                                    ansiedade, durmo melhor e me sinto mais disposto. Isso renova minha fé e esperança.&quot;
                                 </p>
                             </div>
                         </div>
@@ -224,12 +224,12 @@ const Main = () => {
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">MÁRCIA BIAZI</h5>
-                                <p>"Topei compartilhar este depoimento porque acredito na importância do seu trabalho.
+                                <p>&quot;Topei compartilhar este depoimento porque acredito na importância do seu trabalho.
                                     Muitos subestimam a terapia, mas todos enfrentam problemas e nem sempre sabem como lidar com eles.
                                     Se as pessoas entendessem o valor da terapia, a visão sobre os psicólogos seria diferente.
                                     No início da terapia, estava inseguro, mas foi melhor do que eu esperava. É libertador, traz alívio e clareza.
                                     Não há julgamento, é um espaço para se entender. Agradeço por existirem psicólogos como você,
-                                    dedicados a ajudar as pessoas. Obrigado, Edmilson, pelo carinho e compreensão."
+                                    dedicados a ajudar as pessoas. Obrigado, Edmilson, pelo carinho e compreensão.&quot;
                                 </p>
                             </div>
                         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
@@ -18,7 +19,7 @@ const Home = () => {
         <section className="jumbotron">
           <div className=" row p-4 d-flex align-items-center">
             <div className="col-md-6 col-sm-4 d-flex justify-content-center">
-              <img className=" img-fluid rounded-circle perfil" src="perfil.webp" alt="perfil" />
+              <Image className="img-fluid rounded-circle perfil" src="/perfil.webp" width="400" height="400" alt="perfil" />
             </div>
             <div className="col-md-6 col-sm-8">
               <h2>Experiência em atendimento em ansiedade e inteligência emocional.</h2>
@@ -31,9 +32,10 @@ const Home = () => {
 
         <Main />
 
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossOrigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossOrigin="anonymous"></script>
+        <script async src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossOrigin="anonymous"></script>
+        <script async src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous"></script>
+        <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossOrigin="anonymous"></script>
+
 
         <Footer />
       </main>
